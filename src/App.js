a59b7bd1439ccb2c4978d8ffe46c4b7d62a8879d
Maria Claichnet <lutencom@gmail.com>
2023-05-31 18:51:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-This is a test site
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                This is a test site
+                and this is a commited change
+            </header>
+        </div>
+    );
 }
 
 export default App;
